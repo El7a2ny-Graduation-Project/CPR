c8bf892c54bbd3f39dd0ddd6390d41fcb262b72b
FatemaKotb <Fatema.Kotb02@eng-st.cu.edu.eg>
2025-04-29 18:33:08 +0300

    Basic Tracking - Mid Understanding

--- Keypoint Detection/Phase 3 - YOLO/yolov8/test_basicYOLO.py
+++ Keypoint Detection/Phase 3 - YOLO/yolov8/test_basicYOLO.py
@@ -2,4 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO("yolo11n-pose.pt")  # fixed variable name
 
-results = model(source="real.mp4", show=True, save=True, project="runs/detect", name="yolo11n-pose")
+file_path = r"C:\Users\Fatema Kotb\Documents\CUFE 25\Year 04\GP\Spring\El7a2ny-Graduation-Project\CPR\Keypoint Detection\Dataset\Tracking\video_3.mp4"
+
+results = model(source=file_path, show=True, save=True, project="runs/detect", name="yolo11n-pose")
